b79480c3b743febe9cf98c3925385871beb16f21
roy wu <ojinfen@gmail.com>
2024-01-29 15:13:43 -0500

    added vercel web analytic component

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Head from 'next/head'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       
-      <body className={inter.className}>{children}
+      <body className={inter.className}>
+        {children}
+        <Analytics />
       </body>
     </html>
   )
