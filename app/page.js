4db5db2165c1b654ea421e7333ed3a99548be60c
roy wu <ojinfen@gmail.com>
2024-08-08 16:40:25 -0400

    added bd work box

--- app/page.js
+++ app/page.js
@@ -11,6 +11,7 @@ import AboutBox from "../components/AboutBox"
 
 import { TbCoffee } from "react-icons/tb";
 import { motion } from 'framer-motion';
+import BD_Card from "@/components/work-experiences/BD_card"
 
 
 export default function Home() {
@@ -21,7 +22,7 @@ export default function Home() {
 
       {/* ME SECTION - SOCIALS - CONTACT */}
       <section className="pt-4 md:pt-8 lg:pt-10">
-        <MeSection pfp ={profilePic} />
+        <MeSection pfp={profilePic} />
       </section>
 
 
@@ -31,39 +32,59 @@ export default function Home() {
       </section>
 
       <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 1, delay: 0.85 }}
-            >
-      <section className="mx-auto max-w-screen-md">
-        <div className="flex flex-col md:flex-row gap-1 md:gap-6 lg:gap-8 mx-auto">
-          {/* Work Title */}
-          <div className="text-stone-300 font-medium text-sm md:text-base ml-10 mb-1 md:ml-8">
-            projects
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 0.85 }}
+      >
+
+        {/* EXPERIENCE SECTION */}
+        <section className="mx-auto max-w-screen-md pb-12">
+          <div className="flex flex-col md:flex-row gap-1 md:gap-6 lg:gap-8 mx-auto">
+           
+            <div className="text-stone-300 font-medium text-sm md:text-base ml-10 mb-1 md:ml-8">
+              experience
+            </div>
+
+            {/* Work Content */}
+            <div className="flex flex-col gap-4 ml-10 mr-8 md:mr-16 lg:ml-0 lg:mr-124 w-full ">
+
+              <section className="hover:shadow-xl hover:mt-4 hover:mb-4 transition-all transform hover:scale-105 duration-300 rounded-md mr-76 md:mr-14 lg:mr-0 lg:ml-0">
+                <BD_Card />
+              </section>
+
+            </div>
           </div>
+        </section>
 
-          {/* Work Content */}
-          <div className="flex flex-col gap-4 ml-10 mr-8 md:mr-16 lg:ml-0 lg:mr-124 w-full ">
+        <section className="mx-auto max-w-screen-md">
+          <div className="flex flex-col md:flex-row gap-1 md:gap-6 lg:gap-8 mx-auto">
+            {/* Work Title */}
+            <div className="text-stone-300 font-medium text-sm md:text-base ml-10 mb-1 md:ml-8">
+              projects
+            </div>
 
-            <section className="hover:shadow-xl hover:mt-4 hover:mb-4 transition-all transform hover:scale-105 duration-300 rounded-md mr-76 md:mr-14 lg:mr-0 lg:ml-5">
-              <MeddayCard />
-            </section>
+            {/* Work Content */}
+            <div className="flex flex-col gap-4 ml-10 mr-8 md:mr-16 lg:ml-0 lg:mr-124 w-full ">
 
-            <section className="hover:shadow-xl hover:mt-4 hover:mb-4 transition-all transform hover:scale-105 duration-300 rounded-md mr-76 md:mr-14 lg:mr-0 lg:ml-5">
-              <GetAwayCard />
-            </section>
+              <section className="hover:shadow-xl hover:mt-4 hover:mb-4 transition-all transform hover:scale-105 duration-300 rounded-md mr-76 md:mr-14 lg:mr-0 lg:ml-6">
+                <MeddayCard />
+              </section>
 
+              <section className="hover:shadow-xl hover:mt-4 hover:mb-4 transition-all transform hover:scale-105 duration-300 rounded-md mr-76 md:mr-14 lg:mr-0 lg:ml-6">
+                <GetAwayCard />
+              </section>
+
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
 
 
-      {/*FOOTER*/}
-      <section className="flex flex-row mt-8 pb-3 md:mt-8 md:pb-4">
-        <div className="w-max mx-auto text-stone-300 text-3xs md:text-xs opacity-40 flex">
-          made by roy wu with react, tailwind, and lots of coffee <span className="md:ml-1 align-top"><TbCoffee size="21" color="burlywood" /></span>
-        </div>
-      </section>
+        {/*FOOTER*/}
+        <section className="flex flex-row mt-8 pb-3 md:mt-8 md:pb-4">
+          <div className="w-max mx-auto text-stone-300 text-3xs md:text-xs opacity-40 flex">
+            made by roy wu with react, tailwind, and lots of coffee <span className="md:ml-1 align-top"><TbCoffee size="21" color="burlywood" /></span>
+          </div>
+        </section>
 
       </motion.div>
     </main>
---
+++ components/work-experiences/BD_card.jsx
@@ -0,0 +1,188 @@
+"use client"
+
+import { motion } from 'framer-motion';
+
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import { Tooltip } from '@mui/material';
+
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+import { FaAngular } from "react-icons/fa";
+import { SiCsharp } from "react-icons/si";
+import { SiTypescript } from "react-icons/si";
+import { SiDotnet } from "react-icons/si";
+import { BsFiletypeSql } from "react-icons/bs";
+
+
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
+
+export default function BD_Card() {
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    return (
+        <Card sx={{
+            bgcolor: '#282828',
+            fontFamily: 'Inter',
+        }}>
+            <CardHeader
+                sx={{
+                    color: 'white',
+                    fontFamily: 'Inter',
+                    '.MuiCardHeader-title': {
+                        fontSize: '1.15rem',
+                        fontWeight: 'bold',
+                    },
+                    '.MuiCardHeader-subheader': {
+                        fontSize: '0.85rem',
+                        color: '#a7a7a7',
+                    },
+                }}
+
+                avatar={
+                    <Avatar sx={{ bgcolor: 'transparent', borderRadius: '0' }} aria-label="BD Logo">
+                        <img src="/Becton_Dickinson_logo.svg" alt="BD Logo" style={{ width: '100%', height: '100%' }} />
+                    </Avatar>
+                }
+
+                title="Becton Dickinson"
+                subheader={<>Software R&D Intern <br /> Diagnostic Solutions</>}
+            />
+
+            <CardMedia
+                component="img"
+                height="100"
+                image="/COR.png"
+                alt="COR picture"
+            />
+
+            <CardContent>
+                <Typography variant="body2" color="white">
+                    I was a programmer for COR, a medical device that performs automated,
+                    high-throughput molecular diagnostics on patient samples from specimen to result.
+                </Typography>
+            </CardContent>
+
+            <CardActions disableSpacing>
+
+                <Tooltip title="Frontend w/ TypeScript">
+                    <IconButton aria-label="">
+                        <SiTypescript color="#a7a7a7" />
+                    </IconButton>
+                </Tooltip>
+
+                <Tooltip title="Frontend w/ Angular">
+                    <IconButton aria-label="">
+                        <FaAngular color="#a7a7a7" />
+                    </IconButton>
+                </Tooltip>
+
+                <Tooltip title="Backend w/ C#">
+                    <IconButton aria-label="">
+                        <SiCsharp color="#a7a7a7" />
+                    </IconButton>
+                </Tooltip>
+
+                <Tooltip title="Backend w/ .NET">
+                    <IconButton aria-label="">
+                        <SiDotnet color="#a7a7a7" />
+                    </IconButton>
+                </Tooltip>
+
+                <Tooltip title="Database w/ SQL">
+                    <IconButton aria-label="">
+                        <BsFiletypeSql color="#a7a7a7" />
+                    </IconButton>
+                </Tooltip>
+
+
+                <ExpandMore
+                    expand={expanded}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <Tooltip title="Read more" placement="left">
+                        <ExpandMoreIcon style={{ color: '#a7a7a7' }} />
+                    </Tooltip>
+                </ExpandMore>
+
+            </CardActions>
+
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    <div style={{ display: 'flex' }}>
+                        <Typography variant="body2" color="gray" style={{ width: '80px' }}>Company</Typography>
+                        <Typography variant="body2" color="White">
+                            BD - Becton Dickinson
+                        </Typography>
+                    </div>
+
+                    <div style={{ display: 'flex' }}>
+                        <Typography variant="body2" color="gray" style={{ width: '80px' }}>Industry</Typography>
+                        <Typography variant="body2" color="White">
+                            Medical Devices
+                        </Typography>
+                    </div>
+
+                    <div style={{ display: 'flex' }}>
+                        <Typography variant="body2" color="gray" style={{ width: '80px' }}>Role</Typography>
+                        <Typography variant="body2" color="White">
+                            Software R&D Intern
+                        </Typography>
+                    </div>
+
+                    <div style={{ display: 'flex' }}>
+                        <Typography variant="body2" color="gray" style={{ width: '80px' }}>Timeline</Typography>
+                        <Typography variant="body2" color="White">
+                            Aug 2024 - Aug 2024
+                        </Typography>
+                    </div>
+
+                    <br />
+
+                    <div>
+                        <Typography variant="body2" color="gray">Accomplished</Typography>
+                        <Typography variant="body2" color="White">
+                            • Used Angular to implement a feature that displays a table of all specimen samples in COR and allows users to select and unload
+                            samples, enhancing workflow efficiency and sample integrity
+                            <br /><br />
+                            • Created a C# .NET backend service for COR that tracks user activity by logging initiated sample unload procedures
+                            <br /><br />
+                            • Wrote a SQL script for COR that enabled the processing of HPV Dry-Swab Self Collection sample type in US region after FDA
+                            approval of its assay
+                            <br /><br />
+                            • Automated developer environment configurations using Entity Framework, significantly reducing COR simulator startup time
+                            <br /><br />
+                            • Contributed as part of an Agile team, engaging in daily standups, sprint planning, and test-first discussions
+                        </Typography>
+                    </div>
+
+                </CardContent>
+            </Collapse>
+        </Card>
+    );
+}
